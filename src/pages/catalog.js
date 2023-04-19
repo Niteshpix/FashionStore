9767a3bba0f375371e98b644794cd9ed70c076e5
Niteshpix <nitesh@pixlerlab.com>
2023-04-19 17:40:34 +0530

    some style fixes

--- src/pages/catalog.js
+++ src/pages/catalog.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styles from "../styles/product.module.css";
 import axios from "axios";
-import { Card, Col } from "react-bootstrap";
+import { Card, Col, Row } from "react-bootstrap";
 
 function Catalog() {
   const [product, setProduct] = useState([]);
@@ -21,32 +21,36 @@ function Catalog() {
   return (
     <div className={styles.product}>
       <h2>Products</h2>
-      {product.map((prod) => {
-        return (
-          <Col sm={6} md={4} lg={3} key={prod.id}>
-            <Card
-              //onClick={() => handleClick(art?.sys?.id)}
-              style={{ cursor: "pointer", width: "350px" }}
-            >
-              <Card.Img
-                variant="top"
-                src={prod.image.src}
-                style={{ height: "300px", width: "300px" }}
-              />
-              <Card.Body>
-                <Card.Title>{prod.title}</Card.Title>
-                {prod.variants.map((variant, i) => {
-                  return (
-                    <h5 key={i}>
-                      ${variant.price}
-                    </h5>
-                  );
-                })}
-              </Card.Body>
-            </Card>
-          </Col>
-        );
-      })}
+      <Row xs={1} md={2} lg={4} style={{ width: "80%", marginTop: "5px" }}>
+        {product.map((prod) => {
+          return (
+            <Col key={prod.id}>
+              <Card
+                //onClick={() => handleClick(art?.sys?.id)}
+                style={{ cursor: "pointer", width: "310px" }}
+                className="h-100"
+              >
+                <Card.Img
+                  variant="top"
+                  src={prod.image.src}
+                  style={{
+                    height: "300px",
+                    width: "307px",
+                    objectFit: "cover",
+                    objectPosition: "center",
+                  }}
+                />
+                <Card.Body>
+                  <Card.Title>{prod.title}</Card.Title>
+                  {prod.variants.map((variant, i) => {
+                    return <h5 key={i}>${variant.price}</h5>;
+                  })}
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
     </div>
   );
 }
