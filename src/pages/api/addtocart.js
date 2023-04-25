d00377423042ffafdbea6dd4392aa30855d78c4d
Niteshpix <nitesh@pixlerlab.com>
2023-04-25 16:16:28 +0530

    changes

--- src/pages/api/addtocart.js
+++ src/pages/api/addtocart.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { variants } = req.body;
@@ -9,24 +11,24 @@ export default async function handler(req, res) {
         url += ",";
       }
     }
+    try {
+      const result = await axios.post(url,
+        {
+          variants,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Basic ${Buffer.from(
+              `${process.env.SHOPIFY_STORE_API_KEY}:${process.env.SHOPIFY_STORE_API_PASS_TOKEN}`
+            ).toString("base64")}`,
+          },
+        }
+      );
 
-    const result = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Basic ${Buffer.from(
-          `${process.env.SHOPIFY_STORE_API_KEY}:${process.env.SHOPIFY_STORE_API_PASS_TOKEN}`
-        ).toString("base64")}`,
-      },
-      body: JSON.stringify({
-        variants,
-      }),
-    });
-
-    if (result.ok) {
       res.status(200).json({ success: true, url });
-    } else {
-      res.status(result.status).json({ success: false });
+    } catch (error) {
+      res.status(error.response.status).json({ success: false });
     }
   } else {
     res.status(405).json({ message: "Method not allowed" });
